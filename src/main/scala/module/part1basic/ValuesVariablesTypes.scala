fdf8bd4cd18626536edcf584c0b75e74d1475906
Soumya ranjan mishra <67691564+soumyaandroid123@users.noreply.github.com>
2022-11-12 15:54:15 +0530

    Completed the Values, Variables & Types

--- src/main/scala/module/part1basic/ValuesVariablesTypes.scala
+++ src/main/scala/module/part1basic/ValuesVariablesTypes.scala
@@ -1,18 +1,20 @@
 package module.part1basic
 
 object ValuesVariablesTypes extends App {
-  //?? VALS ARE IMMUTABLE
+  // VALS ARE IMMUTABLE
   val x: Int = 4
-  //?? COMPILER CAN INFER TYPES - >
+  // COMPILER CAN INFER TYPES - >
   val x1 = 4
   println(x)
   println(x1)
-  //?? VARS ARE MUTABLE
+  // VARS ARE MUTABLE
   var x2 = 2
   x2 = 1
   x2 += 4
   println(x2)
-
+  // ALL VALS & VARS HAVE TYPES
+  // PREFER VALS OVER VARS
+  // LEARN HOW TO USE BASIC DATA TYPES
   val aBoolean: Boolean = true
   println(aBoolean)
   val aString = "Soumya"
