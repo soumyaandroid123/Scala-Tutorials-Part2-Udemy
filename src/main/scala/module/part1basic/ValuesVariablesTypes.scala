7520fbfcd2817103db3e7017e89b2a0c3cb2b9bb
Soumya ranjan mishra <67691564+soumyaandroid123@users.noreply.github.com>
2022-11-12 15:52:19 +0530

    Completed the Vales, Variables & Types

---
+++ src/main/scala/module/part1basic/ValuesVariablesTypes.scala
@@ -0,0 +1,30 @@
+package module.part1basic
+
+object ValuesVariablesTypes extends App {
+  //?? VALS ARE IMMUTABLE
+  val x: Int = 4
+  //?? COMPILER CAN INFER TYPES - >
+  val x1 = 4
+  println(x)
+  println(x1)
+  //?? VARS ARE MUTABLE
+  var x2 = 2
+  x2 = 1
+  x2 += 4
+  println(x2)
+
+  val aBoolean: Boolean = true
+  println(aBoolean)
+  val aString = "Soumya"
+  println(aString)
+  val aChar: Char = 's'
+  println(aChar)
+  val aLong: Long = 12345678
+  println(aLong)
+  val aShort: Short = 1234
+  println(aShort)
+  val aFloat: Float = 2.34
+  println(aFloat)
+  val aDouble: Double = 3.14
+  println(aDouble)
+}
---
+++ src/main/scala/playground/ScalaPlayground.scala
@@ -0,0 +1,7 @@
+package playground
+
+object ScalaPlayground {
+  def main(args: Array[String]): Unit = {
+    println("I'm ready to learn scala.")
+  }
+}
